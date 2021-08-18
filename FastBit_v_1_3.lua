8775ffb1b8a5668abcb02ae8750179699ceef1ac
Jeremy Behreandt <behreandtjeremy@gmail.com>
2021-08-18 00:44:48 -0500

    Fore- and Back-Color No Alpha Assignment
    When assigning a color to app.fgColor and app.bgColor assign clear black when alpha is zero.

--- FastBit_v_1_3.lua
+++ FastBit_v_1_3.lua
@@ -173,14 +173,16 @@ dlg:shades {
     mode = "pick",
     colors = { Color(255, 255, 255, 255) },
     onclick=function(ev)
+        local clr = ev.color
+        if clr.alpha < 1 then clr = Color(0, 0, 0, 0) end
         if ev.button == MouseButton.LEFT then
-            app.fgColor = ev.color
+            app.fgColor = clr
         elseif ev.button == MouseButton.RIGHT then
             -- Bug where assigning to app.bgColor leads to
             -- unlocked palette color assignment instead.
             -- app.bgColor = ev.color
             app.command.SwitchColors()
-            app.fgColor = ev.color
+            app.fgColor = clr
             app.command.SwitchColors()
         end
     end
